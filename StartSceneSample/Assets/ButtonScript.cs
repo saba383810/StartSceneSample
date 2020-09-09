2c214a239248931ec773b221cfb10747a8d8679e
saba383810 <saba383810@gmail.com>
2020-09-09 21:59:57 +0900

    first commit

---
+++ StartSceneSample/Assets/ButtonScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // ★ここを追加
+
+public class ButtonScript : MonoBehaviour
+{
+    //クリックされた時に呼ばれるプログラムをpublicで定義
+    public void OnClick()
+    {
+        Debug.Log("Sceneを移動！");
+        SceneManager.LoadScene("nextScene");// ★ここを追加
+    }
+}
